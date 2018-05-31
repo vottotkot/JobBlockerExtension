4d88f210f02236ea4e08862bd939c25b78f0d571
vottotkot <ann.kotik@gmail.com>
2018-05-31 14:48:26 -0700

    Update content.js
    Modified  the styling

--- content.js
+++ content.js
@@ -1,42 +1,33 @@
 
-//For Glassdoor Adds buttons in Jl div
-//var x= new NopeButtons('jl');
+// Glassdoor Adds buttons in Jl div which correspond to a job list
 var browser=chrome;
 var jobs = document.getElementsByClassName('jl');
 
 browser.storage.sync.get(['blockedJobs'], function(result){ console.log(result.blockedJobs);
-																var BlockedList=result.blockedJobs;
-																	for (var i = 0, l = jobs.length; i < l; i++) {
-																	 //if job is in a Blocked jobs list, then hide it from search
-																	 var JD=jobs[i].getAttribute('data-id');
-																					 if (BlockedList.includes(JD)){
-																	 									jobs[i].style.display= 'none';	
-
-																	 									}else{
-																	 										var button=document.createElement("button"+i);
-																											 var text= document.createTextNode("NOPE");
-																											 jobs[i].appendChild(button,i);
-																											 button.appendChild(text); 										 	
-	 																										 var y=new SaveOnClick(button);
-
-																	 									};
+var BlockedList=result.blockedJobs;
+for (var i = 0, l = jobs.length; i < l; i++) {
+     //if job is in a Blocked jobs list, then hide it from search, if it is not in blocked list-> add a Hide button into each Job listing
+     var JD=jobs[i].getAttribute('data-id');
+     if (BlockedList.includes(JD)){ 
+	     jobs[i].style.display= 'none';	
+             }else{
+		  var button=document.createElement("button"+i);
+		  var text= document.createTextNode("NOPE");
+                  jobs[i].appendChild(button,i);
+         	  button.appendChild(text); 										 	
+	 	  var y=new SaveOnClick(button);
+		};
 																							}});
 
 function SaveOnClick (buttons){
-	
+	//apparently you cannot append to anything in chrome memory, you need to get the key from memory, modify it and re-write it.
 	buttons.addEventListener ("click", function() {
 		var JD=buttons.parentElement.getAttribute('data-id');
 		this.parentElement.style.display= 'none';
 		browser.storage.sync.get(['blockedJobs'], function(result){ var oldjob=result.blockedJobs;
-																   console.log("oldjob" +oldjob+ "  JD is " + JD);
-																   oldjob.push(JD);
-																   console.log(oldjob);
-																   console.log("oldjob after push" +oldjob);
-																   browser.storage.sync.set({blockedJobs: oldjob}, function() {console.log('Value is set to ' + oldjob )});
-																}
-								);
-
-	
+				        oldjob.push(JD);
+		                        browser.storage.sync.set({blockedJobs: oldjob}, function() {console.log('Value is set to ' + oldjob )});
+		                        });
 	});
 		  
     return this;
